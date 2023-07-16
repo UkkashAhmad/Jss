743a1fc4375d0de4805534cdfe60c6fb053143fc
UkkashAhmad <135998226+UkkashAhmad@users.noreply.github.com>
2023-07-16 02:47:10 -0700

    jss

---
+++ java.js
@@ -0,0 +1,22 @@
+
+const Fajar =2
+var qazadays =5
+var totalQazaRakats =Fajar*qazadays
+console.log("u Have to pray total",totalQazaRakats," fajar rakats for",qazadays,"days");
+const Duhar = 4
+var qazadays =5
+var totalQazaRakats = Duhar*qazadays
+console.log("u have to pray total",totalQazaRakats,"duhar rakats for",qazadays,"days")
+const Asar = 4
+var qazadays =5
+var totalQazaRakats = Asar*qazadays
+console.log("u have to pray total",totalQazaRakats,"asar rakats for",qazadays,"days")
+const Maghrib = 3
+var qazadays =5
+var totalQazaRakats = Maghrib*qazadays
+console.log("u have to pray total",totalQazaRakats,"Maghrib rakats for",qazadays,"days")
+const Esha = 4
+var qazadays =5
+var totalQazaRakats = Esha*qazadays
+console.log("u have to pray total",totalQazaRakats,"Esha rakats for",qazadays,"days")
+
